1442bc175e5791f6da6942892c4433b74a30f9b3
tmtr-withplay <newworldomine@gmail.com>
2026-05-31 13:54:34 +0900

    【新規追加】パッケージ構成を適用したプロジェクトを登録

---
+++ ecapp/ecapp/src/main/java/com/example/ecapp/EcappApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcappApplication.class, args);
+	}
+
+}
---
+++ ecapp/ecapp/src/test/java/com/example/ecapp/EcappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
